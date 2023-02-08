a1b02102982e585a8133163d6d62dca424fcabb5
LeeHoonUk <gns6891@naver.com>
2023-02-08 22:18:23 +0900

    기본화면 구성하기

--- config/urls.py
+++ config/urls.py
@@ -16,6 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from apps.views import *
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    # 메인 화면
+    path('', index, name="index"),
 ]
