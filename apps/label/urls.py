548a41e0a48f694953eaeb1a76faf764aee926dd
LeeHoonUk <gns6891@naver.com>
2023-02-20 22:12:20 +0900

    키워드 리스트 및 삭제 구현

--- apps/label/urls.py
+++ apps/label/urls.py
@@ -4,5 +4,6 @@ from django.urls import path
 urlpatterns = [
     # 키워드 작성
     path("write", label_create, name="l-write"),
-    
+    # 키워드 리스트
+    path("list", label_list, name="l-list"),
 ]
--- apps/label/views.py
+++ apps/label/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from django.core.paginator import Paginator
 
 from apps.models import Labels
 from apps.forms.memo_forms import LabelForm
@@ -16,3 +17,15 @@ def label_create(request):
     labels = Labels.objects.all()
 
     return render(request, "label/create.html", {"form" : form, "labels" : labels, "nav_check" : nav_check})
+
+# 키워드 항목 = 로그인 시 삭제 가능
+def label_list(request):
+
+    nav_check = "sidebar_label"
+
+    page = int(request.GET.get("p", 1))
+
+    label_paginator = Paginator(Labels.objects.order_by("-created_at"), 10)
+    labels = label_paginator.get_page(page)
+
+    return render(request, "label/list.html", {"labels": labels, "nav_check": nav_check})
