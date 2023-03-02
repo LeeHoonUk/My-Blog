4e281cd50d98f595328a47a10c43efcf4610e59e
LeeHoonUk <gns6891@naver.com>
2023-03-02 20:32:06 +0900

    API Test Code 작성

--- apps/tests/test_memos.py
+++ apps/tests/test_memos.py
@@ -1,13 +1,14 @@
-from django.test import TestCase, Client
-from apps.models import Keywords, Users
+from apps.models import Keywords, Users, Memos
 from argon2 import PasswordHasher
+from rest_framework.test import APITestCase, APIClient
 
-
-# 회원 Test Code 작성
-class MemosTestCase(TestCase):
+# 메모 Test Code 작성
+class MemosTestCase(APITestCase):
     def setUp(self):
-        # 라벨 등록
+        # 키워드 등록
         Keywords.objects.create(keyword="Django")
+        Keywords.objects.create(keyword="Flask")
+
         # 아이디 등록
         Users.objects.create(
             username="Test_User",
@@ -15,34 +16,84 @@ class MemosTestCase(TestCase):
             hint="answer",
             user_auth="MEMBER"
         )
+
+        # 메모 등록
+        Memos.objects.create(
+            id=1,
+            title="Test Memo",
+            content="Test Content",
+            writer=Users.objects.get(pk=1)
+        )
+        # 키워드 추가
+        Memos.objects.get(pk=1).keywords.add(Keywords.objects.get(pk=1))
     
-    # 라벨 등록 Test
+    # 키워드 Test
     def test_keyword(self):
-        c = Client()
+        c = APIClient()
 
         # 로그인 성공
         body = {"user_id" : "Test_User", "password" : "1234"}
         c.post("/user/login", body)
 
-        # 라벨을 입력하지 않았을 시
+        # 키워드를 입력하지 않았을 시
         body = {"keyword" : ""}
         request = c.post("/apis/keywords/", body)
 
         self.assertEqual(request.status_code, 412)
 
-        # 라벨 중복 에러
+        # 키워드 중복 에러
         body = {"keyword" : "Django"}
         request = c.post("/apis/keywords/", body)
 
         self.assertEqual(request.status_code, 422)
 
-        # 라벨 등록
-        body = {"keyword" : "Flask"}
+        # 키워드 등록
+        body = {"keyword" : "FastAPI"}
         request = c.post("/apis/keywords/", body)
 
         self.assertEqual(request.status_code, 201)
 
-        # 라벨 삭제
+        # 키워드 삭제
         request = c.delete("/apis/keywords/2/", body)
 
         self.assertEqual(request.status_code, 204)
+
+    # 메모 Test
+    def test_memo(self):
+        c = APIClient()
+
+        # 로그인 성공
+        body = {"user_id" : "Test_User", "password" : "1234"}
+        c.post("/user/login", body)
+
+        # 제목을 입력하지 않았을 경우
+        body = {"title" : "", "content" : "메모 Test"}
+        request = c.post("/apis/memos/", body)
+
+        self.assertEqual(request.status_code, 412)
+
+        # 내용을 입력하지 않았을 경우
+        body = {"title" : "메모 Test", "content" : ""}
+        request = c.post("/apis/memos/", body)
+
+        self.assertEqual(request.status_code, 412)
+
+        # 메모 등록
+        body = {"title" : "메모 Test", "content" : "매모 Test"}
+        request = c.post("/apis/memos/", body)
+
+        self.assertEqual(request.status_code, 201)
+
+        # 메모 수정
+        body = {"title" : "메모 Test", "content" : "매모 Test", "keywords" : [1, 2]}
+        request = c.put("/apis/memos/1/", body)
+
+        self.assertEqual(request.status_code, 201)
+
+        # 메모 삭제
+        request = c.delete("/apis/memos/1/", body)
+
+        self.assertEqual(request.status_code, 204)
+
+
+
